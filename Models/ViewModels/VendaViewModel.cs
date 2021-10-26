7dd27205690a6c7a5593e29a211d428f92ea8999
rzanchetta01 <rzanchetta01@gmail.com>
2021-10-26 20:08:40 -0300

    Criando venda view model

--- Models/ViewModels/VendaViewModel.cs
+++ Models/ViewModels/VendaViewModel.cs
@@ -7,5 +7,12 @@ namespace LojaSapatoKhalai.Models.ViewModels
 {
     public class VendaViewModel
     {
+        public string NomeCliente { get; set; }
+        public string NomeModelo { get; set; }
+        public string NomeFornecedor { get; set; }
+        public string DescicaoModelo { get; set; }
+        public int QuantidadeModelo { get; set; }
+        public DateTime DataCompra { get; set; }
+        public double PrecoCompra { get; set; }
     }
 }
