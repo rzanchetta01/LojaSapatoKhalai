fb2434ee13299a1d70c63ac0bed627c78a40107b
stephania Ferreira <stephania.ferreira@hotmail.com>
2021-10-26 21:40:47 -0300

    ModeloViewModel feita

--- Models/ViewModels/ModeloViewModel.cs
+++ Models/ViewModels/ModeloViewModel.cs
@@ -7,5 +7,16 @@ namespace LojaSapatoKhalai.Models.ViewModels
 {
     public class ModeloViewModel
     {
+        public string Nome { get; set;}
+
+        public List<int> Tamanhos { get; set; }
+
+        public List<string> Cores { get; set; }
+
+        public float Preco { get; set;}
+
+        public string Descricao { get; set; }
+
+        public  List<string> Comentarios { get; set; }
     }
 }
