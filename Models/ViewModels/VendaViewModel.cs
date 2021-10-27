5982a39f6994b231a064cb7c4b09dbd348528689
Samantha1024a-l <86083664+Samantha1024a-l@users.noreply.github.com>
2021-10-27 10:00:57 -0300

    Update VendaViewModel.cs

--- Models/ViewModels/VendaViewModel.cs
+++ Models/ViewModels/VendaViewModel.cs
@@ -7,5 +7,34 @@ namespace LojaSapatoKhalai.Models.ViewModels
 {
     public class VendaViewModel
     {
+       public list <string> codigoRef {get; set; } 
+        
+       public list <int> id_modelo {get; set; } 
+        
+       public list <string> nome_model {get; set; } 
+        
+       public list <string> nome_fornecedor {get; set; }
+        
+        public list <int> tamanho_modelo {get; set; }
+        
+        public list <string> cor_modelo {get; set; }
+        
+        public list <float> valorItem {get; set; }
+        
+        public list <int> quantidade {get; set; }
+        
+        public int id_cliente {get; set; }
+        
+        public string nome_cliente {get; set; }
+        
+        public DateTime dtVenda  {get; set; }
+        
+        public float total  {get; set; }
+        
+        
+        
+
+
+        
     }
 }
