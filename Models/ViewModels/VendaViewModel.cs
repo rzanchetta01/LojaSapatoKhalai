0f6bab44d21521effa7961a995b9b45ba8d889a3
rzanchetta01 <rzanchetta01@gmail.com>
2021-10-26 21:32:28 -0300

    Update VendaViewModel.cs

--- Models/ViewModels/VendaViewModel.cs
+++ Models/ViewModels/VendaViewModel.cs
@@ -8,9 +8,8 @@ namespace LojaSapatoKhalai.Models.ViewModels
     public class VendaViewModel
     {
         public string NomeCliente { get; set; }
-        public string NomeModelo { get; set; }
-        public string NomeFornecedor { get; set; }
-        public string DescicaoModelo { get; set; }
+        public List<string> NomeModelo { get; set; }
+        public List<string> NomeFornecedor { get; set; }
         public int QuantidadeModelo { get; set; }
         public DateTime DataCompra { get; set; }
         public double PrecoCompra { get; set; }
