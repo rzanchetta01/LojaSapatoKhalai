492ffc51548ca0282c1b3e393ed6a60d344f959f
rzanchetta01 <rzanchetta01@gmail.com>
2021-10-31 12:43:53 -0300

    Merge Ronaldo Into Main

--- Models/ViewModels/ClienteViewModel.cs
+++ Models/ViewModels/ClienteViewModel.cs
@@ -11,12 +11,9 @@ namespace LojaSapatoKhalai.Models.ViewModels
         
         public string nome {get; set;}
         
-        public string cpf {get; set;}
-        
-        public string nome {get; set;}
+        public string cpf {get; set;}            
 
         public string sexo {get; set;}
-
-        
+   
     }
 }
